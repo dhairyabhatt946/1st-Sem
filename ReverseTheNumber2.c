664c20979d371b073c96d49063bd62d9757a20ae
dhairyabhatt946 <143011610+dhairyabhatt946@users.noreply.github.com>
2023-09-20 21:36:40 +0530

    Add files via upload

---
+++ ReverseTheNumber2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+    int n, sum=0, rem;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while(n!=0){
+        rem=n%10;
+        sum=sum*10+rem;
+        n=n/10;
+    }
+    printf("%d", sum);
+}
